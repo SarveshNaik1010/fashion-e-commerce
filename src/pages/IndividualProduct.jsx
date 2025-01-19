1cac5dd710b40b218478d7b77230d3a42c88e8e8
Sarvesh Naik <naiksarvesh463@gmail.com>
2025-01-19 19:34:34 +0530

    Cosmetic changes

--- src/pages/IndividualProduct.jsx
+++ src/pages/IndividualProduct.jsx
@@ -22,25 +22,25 @@ function IndividualProduct() {
   return (
     <div className="m-auto my-10 flex w-[80lvw] flex-col items-center justify-center gap-10 md:flex-row">
       <div className="md:w-[50%]">
-        <img src={image} alt={title} className="w-[100%] md:h-[40lvh]" />
+        <img src={image} alt={title} className="w-[100%] md:w-[75%]" />
       </div>
       <div className="space-y-4">
         <p className="title-font text-md text-[28px] font-semibold md:text-[32px]">
           {title}
         </p>
         <p className="text-[16px] text-stone-600">{description}</p>
-        <p className="text-sm text-gray-500">
+        <p className="text-[18px] text-gray-500">
           <span className="text-[16px] font-semibold">Category:</span>{' '}
           {category}
         </p>
-        <div className="flex items-center gap-2 text-[16px]">
-          <p className="text-[16px] text-sm md:text-xl">{`⭐ ${rating.rate}`}</p>
-          <p className="text-[16px] text-sm text-gray-500 md:text-xl">{`(${rating.count})`}</p>
+        <div className="flex items-center gap-2">
+          <p className="text-[18px] md:text-xl">{`⭐ ${rating.rate}`}</p>
+          <p className="text-[18px] text-gray-500">{`(${rating.count})`}</p>
         </div>
+        <p className="title-font text-md text-[24px] font-semibold text-gray-900">
+          $ {price}
+        </p>
         {/* <div className="flex items-center justify-between">
-          <p className="title-font text-md font-semibold text-gray-900 md:text-xl">
-            $ {price}
-          </p>
           {isInCart ? (
             <QuantityButton id={id} currentQuantity={currentQuantity} />
           ) : (
